3074fd76e891687a22179b69a0b65f864edec95b
dream <dream@yeah.net>
2017-04-21 14:38:06 +0800

    webaddr

--- huosdk/conf/domain.inc.php
+++ huosdk/conf/domain.inc.php
@@ -3,8 +3,8 @@ define("HTTP", 'http://');
 define("DOMAIN", '.shouyoucun.cn');
 define("DOCDOMAIN", 'shouyoucun.cn');
 /* define("WEBSITE", HTTP.'demo'.DOMAIN);  */       
-define("WEBSITE", HTTP.'www'.DOMAIN);
-define("SDKSITE", HTTP.'www'.DOMAIN);
-define("ADMINSITE", HTTP.'admin'.DOMAIN);
+define("WEBSITE", HTTP.'client'.DOMAIN);
+define("SDKSITE", HTTP.'client'.DOMAIN);
+define("ADMINSITE", HTTP.'service'.DOMAIN);
 define("DOWNSITE", HTTP.'bt76'.DOMAIN);
 define("BBSSITE", HTTP.'bbs'.DOMAIN);
